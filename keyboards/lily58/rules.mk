9ac88fb97fd4273b3865953f84cdc85fe4297376
baobaozi <work@mingteo.com>
2021-08-10 09:53:33 +1000

    Add myself to users directory for consolidated macros, defs, and tap dances. Utilise in my customised keymaps

--- keyboards/lily58/rules.mk
+++ keyboards/lily58/rules.mk
@@ -9,6 +9,7 @@ COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
+UNICODE_ENABLE = no         # Unicode
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306    # OLED display
